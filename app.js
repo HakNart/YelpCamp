eca79447d5e46d85c924ac124f6613586028b818
Kha Tran <khadieptran@gmail.com>
2018-06-13 12:52:31 -0400

    Create a landing page template for '/' route

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 var express = require("express");
 var app = express();
 
+//Set up view engine
+app.set("view engine", "ejs");
+
 //HomePage route
 app.get("/",function(req, res){
-    res.send("Landing page.");
+    res.render("landing");
 });
 
 app.listen(4000, function(){
